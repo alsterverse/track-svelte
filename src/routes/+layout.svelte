acc22605efa011ce5f79629f107400d786ec2594
Märta Ballardini <marta.ballardini@isotop.se>
2023-04-28 14:56:03 +0200

    css variables

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,18 @@
 <slot />
+
+<style>
+	:root {
+		--fontsize-heading: 64px;
+		--fontsize-heading-mobile: 40px;
+		--fontsize-preamble: 24px;
+		--fontsize-preamble-mobile: 20px;
+		--fontsize-body: 21px;
+		--fontsize-body-mobile: 15px;
+		--fontsize-navigation: 16px;
+		--fontsize-body-small: 15px;
+		--color-foreground1: #ffffff;
+		--color-foreground2: #cccccc;
+		--color-background: #0f1217;
+		--color-stroke: #616161;
+	}
+</style>
