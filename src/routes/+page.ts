dc8f02da2b83330113ad7474e0c558b7b66e8bef
Jim Löfgren <jim.lofgren@alster.se>
2023-06-08 15:41:38 +0200

    wip case list

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -5,15 +5,25 @@ import type { PageLoad } from './$types';
 export const load = (async ({ parent }) => {
 	const { storyblokApi } = await parent();
 
-	const dataStory = await storyblokApi.get(`cdn/stories/home/`, {
-		version: 'draft'
+	const dataStory = await storyblokApi.get(`cdn/stories/`, {
+		version: 'draft',
+		starts_with: 'cases/'
 	});
+	const stories = dataStory.data.stories;
 
-	const story = StoryblokStory.safeParse(dataStory.data.story);
-
-	if (!story.success) throw error(500, 'Could not parse Storyblok story');
+	const parsedStories = stories.reduce((acc: StoryblokStory[], story: StoryblokStory) => {
+		const parsedStory = StoryblokStory.safeParse(story);
+		if (!parsedStory.success) {
+			throw error(500, 'Could not parse Storyblok story ' + parsedStory.error);
+		}
+		if (parsedStory.data.content.component !== 'case') {
+			throw error(500, 'Wrong content');
+		}
+		acc.push(parsedStory.data);
+		return acc;
+	}, []);
 
 	return {
-		story: story.data
+		stories: parsedStories
 	};
 }) satisfies PageLoad;
